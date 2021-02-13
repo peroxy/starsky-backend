c9d85106727608513962d65de356b5d52c1695a3
peroxy <peroxy@users.noreply.github.com>
2021-02-13 17:38:49 +0100

    Added TeamMember and Team repository

--- src/main/java/com/starsky/backend/repository/TeamRepository.java
+++ src/main/java/com/starsky/backend/repository/TeamRepository.java
@@ -1,11 +1,10 @@
 package com.starsky.backend.repository;
 
-import com.starsky.backend.domain.User;
+import com.starsky.backend.domain.Team;
+import com.starsky.backend.domain.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(exported = false)
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
-
