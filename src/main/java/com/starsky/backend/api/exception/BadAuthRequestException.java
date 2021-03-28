471d54bcc7c1949110291e270808217a1f3a5929
peroxy <peroxy@users.noreply.github.com>
2021-03-28 18:19:51 +0200

    Remove not needed BadAuthRequestException class

--- src/main/java/com/starsky/backend/api/exception/BadAuthRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.starsky.backend.api.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class BadAuthRequestException extends AuthenticationException {
-    public BadAuthRequestException(String msg) {
-        super(msg);
-    }
-}
