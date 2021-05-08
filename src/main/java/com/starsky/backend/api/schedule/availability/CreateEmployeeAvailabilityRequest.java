5ac3791ce4dc0ce9e7e867980f0f324818485246
peroxy <peroxy@users.noreply.github.com>
2021-05-08 12:09:29 +0200

    Add fields to create/update employee availability requests

--- src/main/java/com/starsky/backend/api/schedule/availability/CreateEmployeeAvailabilityRequest.java
+++ src/main/java/com/starsky/backend/api/schedule/availability/CreateEmployeeAvailabilityRequest.java
@@ -1,4 +1,42 @@
 package com.starsky.backend.api.schedule.availability;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
 public class CreateEmployeeAvailabilityRequest {
+    @NotNull
+    @JsonProperty("availability_start")
+    @Schema(example = "1617032176.7171679", title = "Epoch timestamp of availability start", implementation = Double.class)
+    private final Instant availabilityStart;
+    @NotNull
+    @JsonProperty("availability_end")
+    @Schema(example = "1617052176.7171679", title = "Epoch timestamp of availability end", implementation = Double.class)
+    private final Instant availabilityEnd;
+    @NotNull
+    @JsonProperty("max_hours_per_shift")
+    @Schema(example = "8", title = "Maximum hours per shift")
+    @Min(1)
+    private final int maxHoursPerShift;
+
+    public CreateEmployeeAvailabilityRequest(Instant availabilityStart, Instant availabilityEnd, int maxHoursPerShift) {
+        this.availabilityStart = availabilityStart;
+        this.availabilityEnd = availabilityEnd;
+        this.maxHoursPerShift = maxHoursPerShift;
+    }
+
+    public Instant getAvailabilityStart() {
+        return availabilityStart;
+    }
+
+    public Instant getAvailabilityEnd() {
+        return availabilityEnd;
+    }
+
+    public int getMaxHoursPerShift() {
+        return maxHoursPerShift;
+    }
 }
--- src/main/java/com/starsky/backend/api/schedule/availability/UpdateEmployeeAvailabilityRequest.java
+++ src/main/java/com/starsky/backend/api/schedule/availability/UpdateEmployeeAvailabilityRequest.java
@@ -1,4 +1,39 @@
 package com.starsky.backend.api.schedule.availability;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.Min;
+import java.time.Instant;
+import java.util.Optional;
+
 public class UpdateEmployeeAvailabilityRequest {
+    @JsonProperty("availability_start")
+    @Schema(example = "1617032176.7171679", title = "Epoch timestamp of availability start", implementation = Double.class)
+    private final Instant availabilityStart;
+    @JsonProperty("availability_end")
+    @Schema(example = "1617052176.7171679", title = "Epoch timestamp of availability end", implementation = Double.class)
+    private final Instant availabilityEnd;
+    @JsonProperty("max_hours_per_shift")
+    @Schema(example = "8", title = "Maximum hours per shift")
+    @Min(1)
+    private final Integer maxHoursPerShift;
+
+    public UpdateEmployeeAvailabilityRequest(Instant availabilityStart, Instant availabilityEnd, Integer maxHoursPerShift) {
+        this.availabilityStart = availabilityStart;
+        this.availabilityEnd = availabilityEnd;
+        this.maxHoursPerShift = maxHoursPerShift;
+    }
+
+    public Optional<Instant> getAvailabilityStart() {
+        return Optional.ofNullable(availabilityStart);
+    }
+
+    public Optional<Instant> getAvailabilityEnd() {
+        return Optional.ofNullable(availabilityEnd);
+    }
+
+    public Optional<Integer> getMaxHoursPerShift() {
+        return Optional.ofNullable(maxHoursPerShift);
+    }
 }
